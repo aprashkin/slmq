c143cbe458632b4475e775efd3ee50aa06fd7f42
aprAshkin <aprAshkin@DESKTOP-V1QV72U>
2023-10-04 11:41:07 +0300

    Добавьте файлы проекта.

---
+++ slmq/Auto.cs
@@ -0,0 +1,136 @@
+﻿using System;
+namespace slmq
+{
+    class Carauto
+    {
+        private string nomer;
+        private double bak;
+        private double rasshod;
+        private double ostatok;
+        private int speed;
+        private double probeg;
+
+        public Carauto(double initialFuel, int initialSpeed)
+        {
+            this.bak = initialFuel;
+            this.speed = initialSpeed;
+        }
+
+        public void Info(string nom, double bak, double ras, int speed, double probeg)
+        {
+            this.nomer = nomer;
+            this.bak = bak;
+            this.rasshod = ras;
+            this.ostatok = bak;
+            this.speed = speed;
+            this.probeg = probeg;
+        }
+
+        public void Out()
+        {
+            Console.WriteLine($"Номер и серия тачки: {nomer}");
+            Console.WriteLine($"Сколько в баке осталось: {ostatok} л");
+            Console.WriteLine($"Сколько требуется бенза на 100 км: {rasshod:F2}");
+            Console.WriteLine($"С какой скоростью летим: {speed:F2} км/ч");
+            Console.WriteLine($"Сколько проехали уже: {probeg:F2} км");
+        }
+
+        public void Zaprawka(double top)
+        {
+            ostatok += top;
+            Console.WriteLine($"Заправились, в нём осталось {ostatok} литров");
+        }
+
+
+        public void Move(int speed)
+        {
+
+            Console.Write("Далеко поедем? (в км ес че) ");
+            double distance = double.Parse(Console.ReadLine());
+
+
+            double kmNalitr = 100 / rasshod;
+            double rashod = distance * (rasshod / 100);
+
+
+
+            if (ostatok >= rashod && ostatok != 0 && speed != 0)
+            {
+                ostatok -= rashod;
+                probeg += distance;
+                Console.WriteLine($"Мы проехали {distance:F2} км со скоростью {this.speed} км/ч. Бенза осталось: {ostatok:F2}\nВсего проехали: {probeg:F2} км");
+                this.speed = speed;
+
+            }
+            else if (ostatok <= rashod && ostatok != 0 && speed != 0)
+            {
+                double distance1;
+                distance1 = ostatok * kmNalitr;
+                probeg += distance1;
+                ostatok = 0;
+                Console.WriteLine($"Мы проехали {distance1:F2} км со скоростью {this.speed} км/ч. Бенза больше нема(. Ласточка наша остановилась...\nОбщий пробег: {probeg:F2} км");
+                speed = 0;
+                this.speed = 0;
+            }
+            else if (ostatok == 0)
+            {
+                Console.WriteLine("В баке пересохло уже.");
+            }
+            else if (speed == 0)
+            {
+                Console.WriteLine("Тапочку в пол прожми да.");
+            }
+        }
+
+        public void Zapravka(double top)//заправка ахахааххаха
+        {
+
+            Console.WriteLine("Желаете дозаправиться? (y or n)");
+            string choise = Console.ReadLine();
+            switch (choise)
+            {
+                case "y":
+                    Console.WriteLine("На сколько литров заправляемся?");
+                    top = int.Parse(Console.ReadLine());
+                    ostatok += top;
+                    Console.Clear();
+
+                    break;
+                case "n":
+                    Console.WriteLine("Ну и толкай дальше сам, я не поеду(");
+                    Console.Clear();
+                    break;
+            }
+        }
+        public void Ostanovka()
+        {
+            speed = 0;
+            Console.WriteLine("По тормозам!!!");
+
+        }
+
+        public void Razgon(int sum_speed)
+        {
+            if (ostatok >= 1.0)
+            {
+                rasshod += 0.1;
+                ostatok -= 1.0;
+                speed += sum_speed;
+
+                Console.WriteLine($"Наш борт набирает скорость, равную  {speed} км/ч. Бензин льется в двигатель, как из шланга.");
+            }
+            else
+            {
+                Console.WriteLine("В баке пересохло, никак быстрее не поедем.");
+            }
+        }
+        public void Tormoz(int sum_speed1)
+        {
+            rasshod -= 0.1;
+            ostatok += 1.0;
+            speed -= sum_speed1;
+
+            Console.WriteLine($"Наш борт замедляется до  {speed} км/ч. Бензин течет медленнее.");
+        }
+    }
+}
---
+++ slmq/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+namespace slmq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Сколько бензина у нас было? (в литрах ес че) ");
+            float Zalili = float.Parse(Console.ReadLine());
+
+            Console.Write("С какой скоростью стартует наш борт? (в километрах/час ес че) ");
+            int Nachal_speed = int.Parse(Console.ReadLine());
+
+            Carauto myCar = new Carauto(Zalili, Nachal_speed);
+            myCar.Info("qwe123", Zalili, 7f, Nachal_speed, 0);
+
+            while (true)
+            {
+                Console.WriteLine("\nЧе делать будем?");
+                Console.WriteLine("1 - Поедем");
+                Console.WriteLine("2 - Остановимся");
+                Console.WriteLine("3 - Анти-ускоримся((");
+                Console.WriteLine("4 - Ускоримся))");
+                Console.WriteLine("5 - Покормим машинку");
+                Console.WriteLine("6 - Паспорт машины");
+                Console.WriteLine("7 - Выйти нафик отсюда\n");
+                ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+                switch (keyInfo.Key)
+                {
+                    case ConsoleKey.D1:
+                        myCar.Move(Nachal_speed);
+                        break;
+
+                    case ConsoleKey.D2:
+                        myCar.Ostanovka();
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.Write("На сколько замедлимся?( (в км/ч): ");
+                        int sum_speed1 = int.Parse(Console.ReadLine());
+                        myCar.Tormoz(sum_speed1);
+                        break;
+
+                    case ConsoleKey.D4:
+                        Console.Write("На сколько ускормися?) (в км/ч): ");
+                        int sum_speed = int.Parse(Console.ReadLine());
+                        myCar.Razgon(sum_speed);
+                        break;
+
+                    case ConsoleKey.D5:
+                        double top = double.Parse(Console.ReadLine());
+                        myCar.Zapravka(top);
+                        break;
+
+                    case ConsoleKey.D6:
+                        myCar.Out();
+                        break;
+
+                    case ConsoleKey.D7:
+                        Console.WriteLine("ББ");
+                        return;
+
+                    default:
+                        Console.WriteLine("ТЫ КУДА ЖМАЕШЬ АЛЕ. ДАВАЙ ПО НОВОЙ МИША, ВСЕ @#&#@!");
+                        break;
+                }
+            }
+        }
+    }
+}
